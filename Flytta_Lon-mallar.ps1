28c96043120f74e23c6089bbdf8755ef339aebc9
chida <christian@damberg.org>
2023-09-19 09:55:19 +0200

    Delete Flytta_Lon-mallar.ps1

--- Flytta_Lon-mallar.ps1
+++
@@ -1,7 +0,0 @@
-﻿$BIGRAM = "VAEENK"
-$Today = (get-date -Format yyyyMMdd)
-
-Remove-Item -Path "D:\Visma\wwwroot\$BIGRAM\PPP\Personec_P_web\Lon\cr\rpt\*"
-Remove-Item -Path "D:\Visma\wwwroot\$BIGRAM\PPP\Personec_AG\CR\rpt\*"
-Write-Output("Robocopy D:\Visma\Install\Backup\$Today\wwwroot\$BIGRAM\PPP\Personec_P_web\Lon\cr\rpt D:\Visma\wwwroot\$BIGRAM\PPP\Personec_P_web\Lon\cr\rpt")
-Write-Output("Robocopy D:\Visma\Install\Backup\$Today\wwwroot\$BIGRAM\PPP\Personec_AG\CR\rpt D:\Visma\wwwroot\$BIGRAM\PPP\Personec_AG\CR\rpt")
