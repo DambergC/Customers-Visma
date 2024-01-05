579a36838fc10f327d5158548a7fdd2318ae8452
chida <christian@damberg.org>
2024-01-05 17:24:00 +0100

    Create DBrestore.ps1
    Work in progress

---
+++ DBrestore.ps1
@@ -0,0 +1,55 @@
+
+if (Get-Module -Name dbatools)
+
+{
+    write-host 'Dbatools installed' -ForegroundColor Green
+}
+
+else
+
+{
+    Install-Module dbatools -Verbose -Force
+    Set-DbatoolsConfig -FullName sql.connection.trustcert -Value $true -Register
+    Set-DbatoolsConfig -FullName sql.connection.encrypt -Value $false -Register
+}
+
+$backuppath = 'D:\DBbackup\'
+$SQLPROD = 'Localhost'
+$SQLTEST = 'Localhost'
+
+
+$file = 'D:\DBRestore.XML'
+$xml = [XML](get-content $file)
+
+$databasesprod = $xml.Configuration.DatabasesProd.db
+
+foreach ($databaseP in $databasesprod)
+
+{
+
+    Write-host "Start backup of $databaseP" -ForegroundColor Green
+    Backup-DbaDatabase -SqlInstance $SQLPROD -Database $databaseP -Path $backuppath -Type Full -CopyOnly -Verify -TimeStampFormat yyyyMMdd
+    
+}
+
+
+$file = 'D:\DBRestore.XML'
+$xml = [XML](get-content $file)
+
+$databasestest = $xml.Configuration.DatabasesTest.db
+$databasesprod = $xml.Configuration.DatabasesProd.db 
+
+$date = get-date -Format yyyyMMdd
+
+foreach ($databaseT in $databasestest)
+
+{
+  
+    Write-host "Start restore of $databaseT from $backuppath$databaseT$date.bak" -ForegroundColor Green
+    #Backup-DbaDatabase -SqlInstance $SQLPROD -Database $database -Path $backuppath -Type Full -CopyOnly -Verify -TimeStampFormat yyyyMMdd
+    #Restore-DbaDatabase -SqlInstance $SQLTEST -DatabaseName $database -Path "$backuppath$database$date.bak"
+  
+
+}
+
+
