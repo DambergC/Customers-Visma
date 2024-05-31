f73834db278f9cb4bfc137645ea7a32d59988dbf
Christian Damberg <Christian.Damberg@cygate.se>
2024-05-31 20:48:43 +0200

    update

--- InstallSupport-PersonecP.TempPoint.ps1
+++ InstallSupport-PersonecP.TempPoint.ps1
@@ -6,40 +6,37 @@
 		This Script supports the work with upgrading Peronec P and task connected to the product
 	
 	.PARAMETER XML
-		Only to be used first time to create a new Scriptconfig.xml. You need to provide BIGRAM
+		A description of the XML parameter.
 	
 	.PARAMETER Backup
-		Backup files for personec p with some exclutions
+		A description of the Backup parameter.
+	
+ 	.PARAMETER CertRights
+		A description of the Backup parameter.
 	
 	.PARAMETER SqlQueries
-		Creates the sql-queries needed for upgrade but also for creating Quick Report users
+		A description of the SqlQueries parameter.
 	
 	.PARAMETER InventorySystem
-		Inventory serivces, installed applications from Visma and existing applications pools
+		A description of the InventorySystem parameter.
 	
 	.PARAMETER InventorySettings
-		Inventory settings from filebackup that might be useful for you during upgrade.
+		A description of the InventorySettings parameter.
 	
 	.PARAMETER InventoryPasswords
-		Extract the passwords from the filebackup
+		A description of the InventoryPasswords parameter.
 	
 	.PARAMETER Password
-		Generate passwords randomly.
+		A description of the Password parameter.
 	
 	.PARAMETER ShutdownServices
-		Shutdown services and the site.
+		A description of the ShutdownServices parameter.
 	
 	.PARAMETER CopyReports
 		A description of the CopyReports parameter.
 	
 	.PARAMETER DBAbackup
 		A description of the DBAbackup parameter.
-	
-	.PARAMETER CertRights
-		Select a cert that you want to set defaultrights to manage private key.
-	
-	.PARAMETER certthumbprint
-		Select a cert and get thumbprint sent to clipboard.
 	
 	.EXAMPLE
 		InstallSupport-PersonecP.ps1 -backup
@@ -50,6 +47,7 @@
 	
 	.EXAMPLE
 		InstallSupport-PersonecP.ps1 -InventoryConfig
+		
 	
 	.EXAMPLE
 		InstallSupport-PersonecP.ps1 -ShutdownServices
@@ -66,7 +64,7 @@
 		Version 2.0 - XML-fil and remove password
 		Version 2.1 - Removed Swedish
 		Version 2.2 - Added scheduler and message broker services to check and stop.
-		Version 2.3 - Added , Scheduler.txt* to exklude in backup
+  		Version 2.3 - Added , Scheduler.txt* to exklude in backup
 #>
 param
 (
@@ -90,10 +88,12 @@ param
 	[Switch]$CopyReports,
 	[Parameter(Mandatory = $false)]
 	[Switch]$DBAbackup,
-	[Parameter(Mandatory = $false)]
-	[Switch]$CertRights,
-	[Parameter(Mandatory = $false)]
+ 	[Parameter(Mandatory = $false)]
+	[Switch]$CertRights, ,
+ 	[Parameter(Mandatory = $false)]
 	[Switch]$certthumbprint
+
+
 )
 
 $checkVersionConfig = '24.5.0'
@@ -113,9 +113,9 @@ if ($XML -eq $true)
 		Add-Type -AssemblyName Microsoft.VisualBasic
 		$bigramtoXML = [Microsoft.VisualBasic.Interaction]::InputBox("Enter BIGRAM", "Enter customer bigram", "BIGRAM")
 		$PPPXML = [Microsoft.VisualBasic.Interaction]::InputBox("Enter PPP Version (SQL)", "VersionNumber PPP", $PPPversionScript)
-		$PUDXML = [Microsoft.VisualBasic.Interaction]::InputBox("Enter PUD Version (SQL)", "VersionNumber PUD", $PUDversionScript)
+  		$PUDXML = [Microsoft.VisualBasic.Interaction]::InputBox("Enter PUD Version (SQL)", "VersionNumber PUD", $PUDversionScript)
 		$PFHXML = [Microsoft.VisualBasic.Interaction]::InputBox("Enter PFH Version (SQL)", "VersionNumber PFH", $PFHversionScript)
-		
+
 		
 		#Create XML
 		$xmlWriter = New-Object System.XMl.XmlTextWriter("$PSScriptRoot\ScriptConfig.XML", $null)
@@ -132,7 +132,7 @@ if ($XML -eq $true)
 		$xmlWriter.WriteElementString("DBscriptPath", "D:\Visma")
 		$xmlWriter.WriteElementString("PPP", "$PPPXML")
 		$xmlWriter.WriteElementString("PUD", "$PUDXML")
-		$xmlWriter.WriteElementString("PFH", "$PFHXML")
+  		$xmlWriter.WriteElementString("PFH", "$PFHXML")
 		$xmlWriter.WriteEndElement() # Configuration endnode
 		$xmlWriter.Flush()
 		$xmlWriter.Close()
@@ -186,7 +186,7 @@ if ($XMLexist -eq $true)
 		
 		Add-Type -AssemblyName Microsoft.VisualBasic
 		$PPPXML = [Microsoft.VisualBasic.Interaction]::InputBox("Enter PPP Version (SQL)", "VersionNumber PPP", $PPPversionScript)
-		$PUDXML = [Microsoft.VisualBasic.Interaction]::InputBox("Enter PUD Version (SQL)", "VersionNumber PUD", $PUDversionScript)
+  		$PUDXML = [Microsoft.VisualBasic.Interaction]::InputBox("Enter PUD Version (SQL)", "VersionNumber PUD", $PUDversionScript)
 		$PFHXML = [Microsoft.VisualBasic.Interaction]::InputBox("Enter PFH Version (SQL)", "VersionNumber PFH", $PFHversionScript)
 		
 		#Create XML
@@ -222,7 +222,7 @@ if ($XMLexist -eq $true)
 		Write-host "CustomerBigram: $BigramXML"
 		Write-host "SQL-verison PPP:$PPPXML"
 		Write-host "SQL-verison PUD:$PUDXML"
-		Write-host "SQL-verison PFH:$PFHXML"
+  		Write-host "SQL-verison PFH:$PFHXML"
 		
 		Add-Type -AssemblyName PresentationCore, PresentationFramework
 		$ButtonType = [System.Windows.MessageBoxButton]::Ok
@@ -934,7 +934,7 @@ if ($DBAbackup -eq $true)
 #Certrights
 if ($Certrights -eq $true)
 {
-	
+
 <#
 .Synopsis
    Short description
@@ -945,30 +945,30 @@ if ($Certrights -eq $true)
 .EXAMPLE
    Another example of how to use this cmdlet
 #>
-	function Set-PermissionCertificate
-	{
-		
-		$Certificate = Get-ChildItem Cert:\LocalMachine\My | Out-GridView -Title 'Select cert' -PassThru
-		
-		$rsaCert = [System.Security.Cryptography.X509Certificates.RSACertificateExtensions]::GetRSAPrivateKey($Certificate)
-		
-		[string]$uniqueName = $rsaCert.key.UniqueName
-		[string]$keyFilePath = "$env:ALLUSERSPROFILE\Microsoft\Crypto\RSA\MachineKeys\$uniqueName"
-		$acl = Get-Acl -Path $keyFilePath
-		
-		$rule1 = new-object security.accesscontrol.filesystemaccessrule 'Visma Services Trusted Users', 'fullcontrol', allow
-		
-		$acl.AddAccessRule($rule1)
-		Set-Acl -Path $keyFilePath -AclObject $acl
-		
-		$rule2 = new-object security.accesscontrol.filesystemaccessrule 'IIS_IUSRS', 'read', allow
-		
-		$acl.AddAccessRule($rule2)
-		Set-Acl -Path $keyFilePath -AclObject $acl
-		
-	}
-	Set-PermissionCertificate
-	
+function Set-PermissionCertificate
+{
+
+    	$Certificate = Get-ChildItem Cert:\LocalMachine\My | Out-GridView -Title 'Select cert' -PassThru
+
+        $rsaCert = [System.Security.Cryptography.X509Certificates.RSACertificateExtensions]::GetRSAPrivateKey($Certificate)
+
+        [string] $uniqueName = $rsaCert.key.UniqueName
+        [string] $keyFilePath = "$env:ALLUSERSPROFILE\Microsoft\Crypto\RSA\MachineKeys\$uniqueName"
+        $acl = Get-Acl -Path $keyFilePath
+        
+        $rule1 = new-object security.accesscontrol.filesystemaccessrule 'Visma Services Trusted Users', 'fullcontrol', allow
+               
+        $acl.AddAccessRule($rule1)
+        Set-Acl -Path $keyFilePath -AclObject $acl
+
+        $rule2 = new-object security.accesscontrol.filesystemaccessrule 'IIS_IUSRS', 'read', allow
+               
+        $acl.AddAccessRule($rule2)
+        Set-Acl -Path $keyFilePath -AclObject $acl
+
+}
+Set-PermissionCertificate
+
 	
 }
 
@@ -979,40 +979,40 @@ if ($Certrights -eq $true)
 #thumbprint
 if ($certthumbprint -eq $true)
 {
-	# Define the starting date for the search
-	$StartDate = Get-Date
-	
-	# Define the certificate path
-	$CertPath = 'Cert:\LocalMachine\my'
-	
-	# Retrieve certificate details
-	$CertsDetail = Get-ChildItem -Path $CertPath -Recurse | Where-Object {
-		$_.PsIsContainer -ne $true
-	} | ForEach-Object {
-		# Calculate the number of days left until expiration
-		$DaysLeft = (New-TimeSpan -Start $StartDate -End $_.NotAfter).Days
-		# Format the expiration date
-		$FinalDate = Get-Date $_.NotAfter -Format 'dd/MM/yyyy hh:mm'
-		# Retrieve intended purposes
-		$Usages = $_.Extensions | Where-Object {
-			$_.Oid.FriendlyName -eq 'Enhanced Key Usage'
-		} | ForEach-Object {
-			$_.Format(0) -join ', '
-		}
-		# Create a custom object with the required details
-		[PSCustomObject]@{
-			Thumbprint	     = $_.Thumbprint
-			Subject		     = $_.Subject
-			ExpireDate	     = $FinalDate
-			DaysRemaining    = $DaysLeft
-			IntendedPurposes = $Usages
-		}
-	} | Out-GridView -PassThru
-	
-	$CertsDetail.thumbprint | Set-Clipboard
-	
-	#endregion
-	
-	
-	
+# Define the starting date for the search
+$StartDate = Get-Date
+
+# Define the certificate path
+$CertPath = 'Cert:\LocalMachine\my'
+
+# Retrieve certificate details
+$CertsDetail = Get-ChildItem -Path $CertPath -Recurse | Where-Object {
+    $_.PsIsContainer -ne $true
+} | ForEach-Object {
+    # Calculate the number of days left until expiration
+    $DaysLeft = (New-TimeSpan -Start $StartDate -End $_.NotAfter).Days
+    # Format the expiration date
+    $FinalDate = Get-Date $_.NotAfter -Format 'dd/MM/yyyy hh:mm'
+    # Retrieve intended purposes
+    $Usages = $_.Extensions | Where-Object {
+        $_.Oid.FriendlyName -eq 'Enhanced Key Usage'
+    } | ForEach-Object {
+        $_.Format(0) -join ', '
+    }
+    # Create a custom object with the required details
+    [PSCustomObject]@{
+        Thumbprint    = $_.Thumbprint
+        Subject       = $_.Subject
+        ExpireDate    = $FinalDate
+        DaysRemaining = $DaysLeft
+        IntendedPurposes = $Usages
+    }
+} | Out-GridView -PassThru
+
+$CertsDetail.thumbprint | Set-Clipboard
+
+#endregion
+
+
+
 }
